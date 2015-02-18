5f43a725cf8596ff7431d4c8716704c9c53539e7
wStockhausen <william.stockhausen@noaa.gov>
2015-02-18 09:23:28 -0800

    Modified parseNum(...) in TextFunctions.R to handle text that evaluates to a vector of numerical expression (e.g., 'log(2:4)'). Incremented version to 1.0.5.

--- R/TextFunctions.R
+++ R/TextFunctions.R
@@ -28,20 +28,25 @@ stripComments<-function(strv,comment='#'){
 }#stripComments
 
 #'
-#'@title Parse a numeric expression from a character string.
+#'@title Parse numeric expressions from a character vector.
 #'
-#'@title Function to parse a numeric expression from a character string.
+#'@title Function to parse numeric expressions from a character vector.
 #'
-#'@param str - character string to parse
+#'@param str - character vector to parse
 #'
-#'@return parsed and evaluated numerical expression
+#'@return parsed and evaluated numerical expressions as a vector
 #'
 #'@export
 #'
 parseNum<-function(str){
-    expr<-parse(text=paste('tmp<-',str[1]))
-    eval(expr);
-    return(tmp);
+    n<-length(str);
+    res<-list();
+    for (i in 1:n){
+        expr<-parse(text=paste('tmp<-',str[i]))
+        eval(expr);
+        res[[i]]<-tmp;
+    }
+    return(unlist(res));
 }
 
 #'
