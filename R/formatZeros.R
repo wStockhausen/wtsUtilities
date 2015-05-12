3d4dc4ea84cb5ec1bcc1f7279775f7862a656c99
wStockhausen <william.stockhausen@noaa.gov>
2015-05-12 15:25:23 -0700

    Revised formatZeros() to coerce inputs to vector before converting them to numeric (so lists can be input). Incremented version to 1.0.10.

--- R/formatZeros.R
+++ R/formatZeros.R
@@ -3,7 +3,7 @@
 #' 
 #' @description Format input vector, replacing leading or trailing blanks with zeros.
 #'       
-#' @param x - numeric vector
+#' @param x - object that can be coerced to a numeric vector using as.numeric(as.vector(x))
 #' @param width - width of formatted numbers
 #' @param format - c-type format string (see "format" description in base R)
 #' @param ...    - additional inputs to "format"
@@ -13,7 +13,7 @@
 #'@export
 #'
 formatZeros<-function(x,width=2,format="d",...){
-    xp<-as.vector(as.numeric(x));
+    xp<-as.numeric(as.vector(x));
     cxp<-format(xp,width=width,format=format,...);
     res<-gsub(" ","0",cxp,fixed=TRUE)
     return(res)
