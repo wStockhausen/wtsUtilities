6d8b9e7af796c7f47ec9b6d693f80fb8a1caaebb
William Stockhausen <william.stockhausen@noaa.gov>
2015-10-29 17:29:38 -0700

    Fixed documentation for standardize().

--- R/standardize.R
+++ R/standardize.R
@@ -1,5 +1,14 @@
 #'
-#'@
+#'@title Standardize a vector by converting to z-scores.
+#'
+#'@description Function to standardize a vector by converting to z-scores.
+#'
+#'@param x - vector to standardize
+#'
+#'@return standardized vector as z-scores (same size as x)
+#'
+#'@export
+#'
 standardize<-function(x){
     mn<-mean(x,na.rm=TRUE);
     sd<-sd(x,na.rm=TRUE);
