6e5f5a184270ace393e0831b7dd583471f0c4f11
wStockhausen <william.stockhausen@noaa.gov>
2017-09-05 08:43:42 -0700

    Added getCaptionsFromGGPlotList().

--- R/Sum.R
+++ R/Sum.R
@@ -4,9 +4,30 @@
 #'@description Convenience function to sum with NAs removed.
 #'
 #'@param x - object to be summed
+#'@param verbose - flg to print diagnostic info
 #'
-#'@details  \code{Sum(x)} is equivalent to \code{sum(x,na.rm=TRUE)}
+#'@details  If x is numeric, then \code{Sum(x)} is equivalent to \code{sum(x,na.rm=TRUE)}. If x is a character vector,
+#'then \code{Sum(x)} returns x[1]. If x is logical, then \code{Sum(x)} is equivalent to \code{any(x,na.rm=TRUE)}
 #'
 #'@export
 #'
-Sum<-function(x){return(sum(x,na.rm=TRUE));}
+Sum<-function(x,verbose=FALSE){
+    if (length(x)==0) {
+        return(0.0);
+    } else if (is.null(x)) {
+        return(0.0);
+    } else if (is.numeric(x)){
+        if (verbose) cat("x is numeric\n");
+        res<-(sum(x,na.rm=TRUE));
+    } else if (is.logical(x)){
+        if (verbose) cat("x is logical\n");
+        res<-sum(as.numeric(x),na.rm=TRUE);
+    } else if (is.character(x)){
+        if (verbose) cat("x is character\n");
+        res<-(x[!is.na(x)])[1];
+    } else if (is.factor(x)){
+        if (verbose) cat("x is factor\n");
+        res<-as.character(x)[1];
+    }
+    return(res);
+}
---
+++ R/getCaptionsFromGGPlotList.R
@@ -0,0 +1,19 @@
+#'
+#' @title Extract captions from list of plots in printGGList format
+#' 
+#' @description Function to extract captions from list of plots in printGGList format.
+#' 
+#' @param x - list of plots in printGGList format, with captions as list names
+#' 
+#' @details Uses \code{\stringr::str_trim} to trim leading and trailing whitespace.
+#' 
+#' @export
+#' 
+getCaptionsFromGGPlotList<-function(plts){
+    x<-names(plts);
+    x<-gsub("Figure &&figno. ","",x,fixed=TRUE);
+    x<-gsub("Figure &&fno. ","",x,fixed=TRUE);
+    x<-gsub("\n","",x,fixed=TRUE);
+    x<-stringr::str_trim(x,side="both")
+    return(x);
+}
