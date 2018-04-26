4ea7e7e2fff5a30caec4b3d60520c32c8f088841
wStockhausen <william.stockhausen@noaa.gov>
2018-04-26 13:32:33 -0700

    Revised deleteCols(). Incremented version to 2018.04.26.

--- R/deleteCols.R
+++ R/deleteCols.R
@@ -4,12 +4,13 @@
 #' 
 #' @param tbl - dataframe or matrix from which to delete columns
 #' @param cols - names of column to delete
+#' @param debug - T/F to print debugging info
 #' 
 #' @return dataframe or matrix with columns removed
 #' 
 #'@export
 #'
-deleteCols<-function(tbl,cols){
+deleteCols<-function(tbl,cols,debug=FALSE){
     tblp<-tbl;
     if (length(cols)>0) {
         #loop through columns to delete and find matches with table names
@@ -18,13 +19,15 @@ deleteCols<-function(tbl,cols){
             cat('deleteCols:\ninput table names ',nms,'\n');
             cat('columns to delete: (',paste(cols,collapse=","),')\n');
         }
-        res<-rep(TRUE,length(nms));
-        for (c in 1:length(cols)) {
-            res<-res&(nms!=cols[c]);
-        }
+        res<-nms[!(nms %in% cols)];
         if (debug) cat('Selection: \n',res,'\n');
+        # res<-rep(TRUE,length(nms));
+        # for (c in 1:length(cols)) {
+        #     res<-res&(nms!=cols[c]);
+        # }
+        # if (debug) cat('Selection: \n',res,'\n');
         #keep columns that don't match names to be deleted
-        if (debug) cat('Selected columns to keep = ',nms[res],'\n');
+        if (debug) cat('Selected columns to keep = ',res,'\n');
         tblp<-tbl[,res];
         if (debug) cat('output table names: \n',names(tblp),'\nfinished deleteCols');
     }
