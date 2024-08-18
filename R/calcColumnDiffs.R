56bef90d3e61589ccef5166253355e4fae7484dd
William Stockhausen <william.stockhausen@noaa.gov>
2024-08-18 10:11:50 -0700

    2024.08.15. Improved 'calcColumnDiffs' to specify base column and added option to keep the base column.

--- R/calcColumnDiffs.R
+++ R/calcColumnDiffs.R
@@ -7,22 +7,28 @@
 #' @param col - column name for base 
 #' @param cols - vector of column names to compute by-row differences for (or NULL)
 #' @param adjacent - flag to compute differences between "adjacent" columns (default=FALSE)
+#' @param dropCols - flag to drop original columns involved in differences (default=TRUE)
+#' @param dropBase - flag to drop base column involved in differences (default=FALSE)
 #' 
-#' @return copy of input dataframe with extra columns representing column differences
+#' @return copy of input dataframe, with columns inv with extra columns representing column differences
 #' 
 #' @details
-#' If \code{cols} is NULL, then differences will be commputed as if \code{cols} was 
+#' If \code{cols} is NULL, then differences will be computed as if \code{cols} was 
 #' the vector of names of columns to the "right" of \code{col}. If \code{adjacent=FALSE}, 
 #' then all differences are relative to \code{col} as the base. Otherwise, after the 
-#' first difference, the base column is sequentially moves through those in \code{cols}.
+#' first difference, the base column is sequentially moved through those in \code{cols}.
+#' 
+#' @importFrom dplyr select 
+#' @importFrom tidyselect any_of
 #' 
 #' @export
 #' 
-calcColumnDiffs<-function(dfr,col,cols=NULL,adjacent=FALSE){
+calcColumnDiffs<-function(dfr,col,cols=NULL,adjacent=FALSE,dropCols=TRUE,dropBase=FALSE){
   nms = names(dfr);
   idcol = which(nms==col)
   if (is.null(cols)){
     idcols = (idcol+1):length(nms);
+    cols   = nms[idcols];
   } else {
     idcols = which(nms %in% cols)
   }
@@ -34,5 +40,9 @@ calcColumnDiffs<-function(dfr,col,cols=NULL,adjacent=FALSE){
     tmp[[colnm]] = tmp[[id]]-tmp[[base]];
     if (adjacent) base = id;#--set base to current column
   }
+  if (dropCols) 
+    tmp = tmp |> dplyr::select(!tidyselect::any_of(cols));
+  if (dropBase) 
+    tmp = tmp |> dplyr::select(!tidyselect::any_of(col));
   return(tmp);
 }
