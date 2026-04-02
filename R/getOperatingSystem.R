d5aff6c673a96604e2f8c48c3844719911903905
William Stockhausen <william.stockhausen@noaa.gov>
2026-04-02 07:59:29 -0700

    Version 2026.04.02. Added isOSX, isWin, changed getOperatingSystem to return "OSX", not "MacOSX" if on a Mac.

--- R/getOperatingSystem.R
+++ R/getOperatingSystem.R
@@ -3,22 +3,57 @@
 #'
 #'@description Function that returns the operating system name.
 #'
-#'@return Either 'MacOSX' or 'Windows'
+#'@return Either 'OSX' or 'Windows'
 #'
-#'@details None.
+#'@details Checks `Sys.info()[['sysname']]` for platform name and returns 'Win' 
+#'if lower case platform name is 'windows', 'OSX' if 'darwin'.
 #'
 #'@export
 #'
 getOperatingSystem<-function(){
     #check the operating platform
-    MacOSX<-'MacOSX';
+    MacOSX<-'OSX';
     Win<-'Windows';
-    plat<-Sys.info()[['sysname']];
 #    cat(plat,'\n')
-    if (tolower(plat)=='darwin'){
+    if (isOSX()){
         platform<-MacOSX;
-    } else if (tolower(plat)=='windows') {
+    } else if (isWin) {
         platform<-Win;
+    } else {
+        stop("Operating system '",Sys.info()[['sysname']],"' not recognized!");
     }
     return(platform);
 }
+
+#'
+#'@title Test if operating system is OSX.
+#'
+#'@description Function that returns TRUE if the operating system is OSX, FALSE otherwise.
+#'
+#'@return TRUE if the operating system is OSX, FALSE otherwise
+#'
+#'@details Checks `Sys.info()[['sysname']]` for platform name and returns TRUE
+#'if the lower case platform name is 'darwin', FALSE otherwise.
+#'
+#'@export
+#'
+isOSX<-function(){
+    ifelse(tolower(Sys.info()[['sysname']])=="darwin",TRUE,FALSE)
+}
+
+#'
+#'@title Test if operating system is Windows.
+#'
+#'@description Function that returns TRUE if the operating system is Windows, FALSE otherwise.
+#'
+#'@return TRUE if the operating system is Windows, FALSE otherwise
+#'
+#'@details Checks `Sys.info()[['sysname']]` for platform name and returns TRUE
+#'if the lower case platform name is 'windows', FALSE otherwise.
+#'
+#'@export
+#'
+isWin<-function(){
+    ifelse(tolower(Sys.info()[['sysname']])=="windows",TRUE,FALSE)
+}
+
